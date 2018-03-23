d71d2b7e8ef83c03375a8da7b74c6b6712fde462
33alien <bogdan4225@gmail.com>
2018-03-23 20:02:10 +0200

    3

---
+++ l2_vars_and_types/list.py
@@ -0,0 +1,7 @@
+serial_number = ["1n2k3f4gjnlihmn", "ij23fdsg33ew2", "kg838516298gw", "98797tghqgwutw", "liuoihw7q63t3d"]
+print(serial_number[0])
+print(serial_number[1])
+print(serial_number[2])
+print(serial_number[3])
+print(serial_number[4])
+print(type(serial_number))
