e7092a476510d4d69260afe1685c2c80c49dc8dd
33alien <bogdan4225@gmail.com>
2018-03-23 22:22:09 +0200

    6

---
+++ l2_vars_and_types/input, output.py
@@ -0,0 +1,9 @@
+name = input("What is your name?")                  # Enter your name
+last_name = input("What is your last name?")        # Enter your last name
+phone_number = input("Your phone number?")
+"""
+Enter your phone number,
+example: +380()___ __ __
+"""
+group = input("What is your group?")                # Only numeric
+print(f"Your name is {name}, your last name is {last_name}, your phone number is{phone_number}, your group is{group}")
