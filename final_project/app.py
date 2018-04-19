6f30f0f02fa7cc1f81d8e8f441875161e2fea0d6
33alien <bogdan4225@gmail.com>
2018-04-20 01:29:34 +0300

    final project 0.1

--- final_project/app.py
+++ final_project/app.py
@@ -2,22 +2,23 @@ import csv
 
 from final_project.utils._1 import *
 
-ls = []
+header = ["Name", "Phone", "Exp_day", "Card"]
+records = []
+
 for i in range(100):
-    ls.append(generated_input())
+    records.append(createRecord())
 
-with open("cardholders.csv", "w", newline= "") as cardholders_csv:
-    header = ["Name", "Phone", "Exp_day", "Card"]
-    writer = csv.DictWriter(cardholders_csv, fieldnames= header)
-    writer.writeheader()
-    for i in range(len(ls)):
-        if not check_credents(ls[i]):
-            writer.writerow({"Name": ls[i].get("Name"), "Phone": ls[i].get("Phone"), "Exp_day": ls[i].get("Exp_day"), "Card": ls[i].get("Card")})
+cardholdersFile = open("cardholders.csv", "w")
+cardholdersFile = csv.DictWriter(cardholdersFile, fieldnames=header)
+cardholdersFile.writeheader()
+cardholdersErrorFile = open("cardholders_error.csv", "w")
+cardholdersErrorFile = csv.DictWriter(cardholdersErrorFile, fieldnames=header)
+cardholdersErrorFile.writeheader()
 
-with open("cardholders_error.csv", "w", newline="") as cardholders_error_csv:
-    header = ["Name", "Phone", "Exp_day", "Card"]
-    writer = csv.DictWriter(cardholders_error_csv, fieldnames= header)
-    writer.writeheader()
-    for i in range(len(ls)):
-        if check_credents(ls[i]):
-            writer.writerow({"Name": ls[i].get("Name"), "Phone": ls[i].get("Phone"), "Exp_day": ls[i].get("Exp_day"), "Card": ls[i].get("Card")})
+for i in range(len(records)):
+    if not checkCredents(records[i]):
+        cardholdersFile.writerow({"Name": records[i].get("Name"), "Phone": records[i].get("Phone"),
+                                  "Exp_day": records[i].get("Exp_day"), "Card": records[i].get("Card")})
+    else:
+        cardholdersErrorFile.writerow({"Name": records[i].get("Name"), "Phone": records[i].get("Phone"),
+                                       "Exp_day": records[i].get("Exp_day"), "Card": records[i].get("Card")})
--- final_project/utils/_1.py
+++ final_project/utils/_1.py
@@ -5,30 +5,40 @@ import string
 import re
 
 
+def createRecord():
+    cardholders = {"Name": createPerson(), "Phone": createPhoneNumber(),
+                   "Exp_day": createDate(), "Card": createCardNumber()}
+    return cardholders
 
-#firstName = ("Aleksandr", "Andrey", "Gleb", "Yegor", "Igor", "Kirill", "Leonid")
-lastName = ("Polischuk", "Voron", "Glasunov", "Smolenskiy", "Lifecell", "Petrov", "Ivanov")
 
-error_elm_phone = ("[", "-", "]", " ", str(random.choice(string.ascii_lowercase + string.ascii_uppercase)))
-error_elm_card = ("-", " ", str(random.choice(string.ascii_lowercase + string.ascii_uppercase)))
-#code = "+380"
-code = ("+7", "+380")
-number = (95, 66, 50, 44, 63, 98, 96, 97, 44)
-
-def generated_input():
+def createPerson():
+    firstName = ("Aleksandr", "Andrey", "Gleb", "Yegor", "Igor", "Kirill", "Leonid")
+    lastName = ("Polischuk", "Voron", "Glasunov", "Smolenskiy", "Lifecell", "Petrov", "Ivanov")
     person = (random.choice(firstName) + " " + random.choice(lastName))
-    #########################
+    return person
+
+def createPhoneNumber():
+    errorPhoneSymbol = ("[", "-", "]", " ", str(random.choice(string.ascii_lowercase + string.ascii_uppercase)))
+    code = ("+790", "+380")
+    number = (95, 66, 50, 44, 63, 98, 96, 97, 44)
+
     phone = list(f"{random.choice(code)}{random.choice(number)}{''.join([random.choice(digits) for x in range(7)])}")
     if random.randint(1, 50) == 1:
-        phone[random.randint(1, len(phone)-1)] = random.choice(error_elm_phone)
+        phone[random.randint(1, len(phone)-1)] = random.choice(errorPhoneSymbol)
     phone = ''.join(phone)
+    return phone
 
+def createDate():
     mounth = str(random.randint(1, 12))
     if len(mounth) == 1:
         mounth = '0'+ (mounth)
 
     yy = str(random.randint(18, 21))
-    exp_day = mounth + '/' + yy
+    date = mounth + '/' + yy
+    return date
+
+def createCardNumber():
+    errorCardSymbol = ("-", " ", str(random.choice(string.ascii_lowercase + string.ascii_uppercase)))
 
     card = list(''.join(choice(digits) for i in range(4)) +
                 ''.join(choice(digits) for i in range(4)) +
@@ -36,21 +46,16 @@ def generated_input():
                 ''.join(choice(digits) for i in range(4)))
 
     if random.randint(1, 100) == 1:
-        card[random.randint(0, len(card)-1)] = random.choice(error_elm_card)
+        card[random.randint(0, len(card)-1)] = random.choice(errorCardSymbol)
     card = ''.join(card)
     card = card[0:4] + ' ' + card[4:8] + ' ' + card[8:12] + ' ' + card[12:16]
-    cardholders = {"Name": person, "Phone": phone, "Exp_day": exp_day, "Card": card}
-    return cardholders
+    return card
 
-
-def generatedPerson()
-
-
-
-def check_credents(rand_cred):
-    phone_cred = re.search(r'[a-zA-Z\[\]\-\ ]', rand_cred.get("Phone"))
-    card_cred = re.search(r'[a-zA-Z\ ]', (rand_cred.get("Card")[0:4]+rand_cred.get("Card")[5:9]+rand_cred.get("Card")[10:14]+rand_cred.get("Card")[15:19]))
-    if phone_cred or card_cred:
+def checkCredents(object):
+    phoneCredents = re.search(r'[a-zA-Z\[\]\-\ ]', object.get("Phone"))
+    cardCredents = re.search(r'[a-zA-Z\ ]', (object.get("Card")[0:4] + object.get("Card")[5:9]
+                                             + object.get("Card")[10:14] + object.get("Card")[15:19]))
+    if phoneCredents or cardCredents:
         return True
     else:
         return False
